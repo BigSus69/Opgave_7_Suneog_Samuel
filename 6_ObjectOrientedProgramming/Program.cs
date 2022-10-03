b3b01c7852c61c15e25b618ab86acde8a7e147e3
Sune <sune171205@gmail.com>
2022-10-03 14:57:58 +0200

    En lille smule trolleri :)

---
+++ 6_ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+* Todays work will be refactoring the Car, Door and Tire class into seperate files
+* and laying the ground work for the next assignment
+*/
+
+//Instantiating a Car class / Creating an object of type Car
+Car car1 = new Car("BMW");
+Car car2 = new Car("Toyota", 2, 8, 25);
+
+Console.ReadKey();
