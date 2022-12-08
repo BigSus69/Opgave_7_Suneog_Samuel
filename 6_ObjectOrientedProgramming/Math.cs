be97376baf75c4e173357cbd42fc54be9339f310
Samuel <samuel@families.dk>
2022-12-08 09:54:23 +0100

    Math måske virke måske ikke

--- 6_ObjectOrientedProgramming/Math.cs
+++ 6_ObjectOrientedProgramming/Math.cs
@@ -3,7 +3,39 @@ public class Math
 
     public Math()
     {
+            void Text(string text, int time = 50) 
+    { 
+        foreach (char c in text)
+         { 
+            Console.Write(c); 
+            Thread.Sleep(time); 
+            } 
+        }
+
         Colors c = new Colors();
+        Console.WriteLine("hej");
+        string mathnumb = ("");
+        string colorboi = mathnumb;
+        switch (mathnumb){
+        
+            case "Gul":
+            Text("det bliver så 50000000kr");
+            break;
+
+            case "Blå":
+            Text("ok en blå bil det koster 69696969kr");
+            break;
+
+            case "Grøn":
+            Text("what en bozo en grøn bil frfr? nå men 1234567kr");
+            break;
+
+            case "Pink":
+            Text("sheeeeeeeeeeeesh pink stor rabat så 3kr");
+            break;
+
+        }
+        
     }
 
 }
